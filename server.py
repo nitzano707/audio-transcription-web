b83c90cf6032a98fa81989f5e37a30404be8f1c7
nitzano707 <nitzan.orot@gmail.com>
2024-10-26 20:27:41 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import os
 import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # הוספת תמיכה ב-CORS לכל המסלולים
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
